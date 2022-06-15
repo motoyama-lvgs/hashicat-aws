d616a67665a6ac7347fbc379acbfb8c91578fdfd
motoyama-lvgs <system.takahiro.motoyama@gmail.com>
2022-06-15 06:39:22 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,4 @@ module "s3-bucket" {
   source  = "app.terraform.io/motoyama-lvgs/s3-bucket/aws"
   version = "2.2.0"
   bucket_prefix = "takahiro_motoyama"
-  # insert the 5 required variables here
 }
